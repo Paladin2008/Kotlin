47c976aa9c628e65444079ff05c6b916cca8ba34
Vladimir <ligtknow2008@gmail.com>
2023-11-15 23:17:32 +0500

    Add lessons two, task one

---
+++ src/main/kotlin/TaskOne.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val person1 = 3
+    val person2 = 4
+    val person3 = 3
+    val person4 = 5
+
+    val sum = person1 + person2 + person3 + person4
+    val medium: Double = sum / 4.00
+
+    println(medium)
+}
